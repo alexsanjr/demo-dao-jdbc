b6dfad4d94bcaf74f31e153d7aea6b015c1b17a4
Alex Sandro <alex.sandro.1199@gmail.com>
2023-02-03 10:16:54 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,5 +31,11 @@ public class Program {
         sellerDao.insert(newSeller);
         System.out.println( "Inserted! new id = " + newSeller.getId());
 
+        System.out.println("\n=== TEST 5: seller update =====");
+        seller = sellerDao.findById(1);
+        seller.setName("Marta Wayne");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
+
     }
 }
